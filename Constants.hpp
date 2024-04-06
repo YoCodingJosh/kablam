4ca45d1e64b5c2640eb7031edd6b285b1bf7e76f
Josh Kennedy <976307+YoCodingJosh@users.noreply.github.com>
2024-04-06 08:34:27 -0500

    Fix file encoding warning for copyright symbol

--- Constants.hpp
+++ Constants.hpp
@@ -10,8 +10,7 @@
 #pragma once
 
 constexpr auto GAME_TITLE = "Kablam!";
-constexpr auto GAME_COPYRIGHT = "Copyright © 2024 Josh Kennedy";
-
+constexpr auto GAME_COPYRIGHT = "Copyright \xA9 2024 Josh Kennedy";
 constexpr auto SCREEN_WIDTH = 1280;
 constexpr auto SCREEN_HEIGHT = 960;
 
