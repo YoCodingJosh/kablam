0d5b2ff66d7d81d490ed788702639cf000996997
Josh Kennedy <976307+YoCodingJosh@users.noreply.github.com>
2024-05-26 14:30:09 -0500

    fix windows build

--- Main.cpp
+++ Main.cpp
@@ -7,9 +7,19 @@
  * Copyright (C) 2023 Josh Kennedy.
  */
 
+#if _WIN32
+// not sure why visual studio suddenly broke, this fixes it.
+#define SDL_MAIN_HANDLED
+#endif
+
+#include <SDL2/SDL.h>
+
 #include "Game.hpp"
 
 int main(int argc, char* argv[])
 {
+#if _WIN32
+    SDL_SetMainReady();
+#endif
     return Game::get()->run();
 }
